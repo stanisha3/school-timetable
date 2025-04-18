13e0baba4df55f0b72566624a11cf36a14cec578
stanisha3 <143798508+stanisha3@users.noreply.github.com>
2025-04-18 11:36:21 +0530

    Add files via upload

--- routers/teachers.py
+++ routers/teachers.py
@@ -7,19 +7,6 @@ from schemas import TeacherCreate
 
 router = APIRouter(prefix="/teachers", tags=["Teachers"])
 
-def alter_teachers_table():
-    print("Running alter table for teachers...")
-    try:
-        with engine.begin() as conn:
-            conn.execute(text(""" ALTER TABLE teachers ADD COLUMN IF NOT EXISTS email VARCHAR UNIQUE; """))
-            conn.execute(text(""" ALTER TABLE teachers ADD COLUMN IF NOT EXISTS hashed_password VARCHAR;"""))
-        print("✅ Alteration complete.")
-    except Exception as e:
-        print("❌ Error during ALTER:", e)
-
-
-alter_teachers_table()
-
 def get_db():
     db = SessionLocal()
     try:
@@ -33,7 +20,7 @@ def get_teachers(db: Session = Depends(get_db)):
 
 @router.post("/")
 def create_teacher(teacher: TeacherCreate, db: Session = Depends(get_db)):
-    new_teacher = Teacher(**teacher.dict())  # Ensure you're converting the schema to a model
+    new_teacher = Teacher(**teacher.dict())  
     db.add(new_teacher)
     db.commit()
     db.refresh(new_teacher)
