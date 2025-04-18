4f59fac64008c8ba4d9dc908059d59c8c40f6593
bhavya2525 <97285889+bhavya2525@users.noreply.github.com>
2025-04-18 11:59:29 +0530

    Add files via upload

--- routers/teachers.py
+++ routers/teachers.py
@@ -7,6 +7,8 @@ from schemas import TeacherCreate
 
 router = APIRouter(prefix="/teachers", tags=["Teachers"])
 
+
+
 def get_db():
     db = SessionLocal()
     try:
@@ -20,7 +22,7 @@ def get_teachers(db: Session = Depends(get_db)):
 
 @router.post("/")
 def create_teacher(teacher: TeacherCreate, db: Session = Depends(get_db)):
-    new_teacher = Teacher(**teacher.dict())  
+    new_teacher = Teacher(**teacher.dict())  # Ensure you're converting the schema to a model
     db.add(new_teacher)
     db.commit()
     db.refresh(new_teacher)
