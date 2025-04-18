65383db662a32381bda96bb3bf21ba21e72d195d
stanisha3 <143798508+stanisha3@users.noreply.github.com>
2025-04-18 11:36:54 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -4,19 +4,15 @@ from database import engine, create_tables
 from base import Base  
 from models import Teacher, Class, Subject, Timetable  
 from routers import teachers, timetable, subjects
-from routers.teachers import alter_teachers_table  
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     print("Creating tables...")
     create_tables()
-    print("Running ALTER on teachers...")
-    alter_teachers_table()
     print("Setup complete.")
     yield
 
 app = FastAPI(lifespan=lifespan)
-
 
 app.include_router(teachers.router)
 app.include_router(subjects.router)
