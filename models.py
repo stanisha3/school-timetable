5089d45b7c6d443a2dff57355916031dba17adaf
stanisha3 <143798508+stanisha3@users.noreply.github.com>
2025-05-23 18:57:44 +0530

    Add files via upload

--- models.py
+++ models.py
@@ -1,41 +1,18 @@
 from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.orm import relationship
-from sqlalchemy import ForeignKey
-from base import Base
+from database import Base
 
-class Teacher(Base):
-    __tablename__ = "teachers"
+class Assignment(Base):
+    __tablename__ = "assignments"
     id = Column(Integer, primary_key=True, index=True)
-    name = Column(String, unique=True, index=True)
-    email = Column(String, unique=True, nullable=True)
-    hashed_password = Column(String, nullable=True)
-
-
-    subjects = relationship("Subject", back_populates="teacher")
-
-class Class(Base):
-    __tablename__ = "classes"
-    id= Column(Integer, primary_key=True, index=True)
-    name= Column(String, unique=True)
-
-    timetables = relationship("Timetable", back_populates="class_")
-
-class Subject(Base):
-    __tablename__ = "subjects"
-    id=Column(Integer, primary_key=True, index=True)
-    name= Column(String, index=True)
     teacher_id = Column(Integer, ForeignKey("teachers.id"))
-
-    teacher = relationship("Teacher", back_populates="subjects")
-    timetables = relationship("Timetable", back_populates="subject")
-
-class Timetable(Base):
-    __tablename__ = "timetables"
-    id= Column(Integer, primary_key=True, index=True)
-    class_id = Column(Integer, ForeignKey("classes.id"), index=True)
-    day = Column(String)
-    period = Column(Integer)
     subject_id = Column(Integer, ForeignKey("subjects.id"))
-    
-    class_ = relationship("Class", back_populates="timetables")
-    subject = relationship("Subject", back_populates="timetables")
+    class_id = Column(Integer, ForeignKey("classes.id"))
+
+class SchoolSettings(Base):
+    __tablename__ = "school_settings"
+    id = Column(Integer, primary_key=True, index=True)
+    working_days = Column(Integer)
+    periods_per_day = Column(Integer)
+    period_duration_minutes = Column(Integer)
+    school_start_time = Column(Integer)
--- schemas.py
+++ schemas.py
@@ -12,3 +12,33 @@ class ClassCreate(BaseModel):
 class SubjectCreate(BaseModel):
     name: str
     teacher_id: int
+
+class AssignmentBase(BaseModel):
+    teacher_id: int
+    subject_id: int
+    class_id: int
+
+class AssignmentCreate(AssignmentBase):
+    pass
+
+class Assignment(AssignmentBase):
+    id: int
+
+    class Config:
+        orm_mode = True
+
+class SchoolSettingsBase(BaseModel):
+    working_days: int
+    periods_per_day: int
+    period_duration_minutes: int
+    school_start_time: int
+
+class SchoolSettingsCreate(SchoolSettingsBase):
+    pass
+
+class SchoolSettings(SchoolSettingsBase):
+    id: int
+
+    class Config:
+        orm_mode = True
+
